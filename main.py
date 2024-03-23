5e05151a63d9e9898001562ce7b5287e04f6935c
sevenchords <sevenchords@local>
2024-03-23 22:49:08 +0100

    folder checking and menu for four kings machine games

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import os
+
+DATA_PATHS_ = ['data',
+              'data/fk',
+              'data/fk/slots',
+              'data/fk/slots/888',
+              'data/fk/slots/circus',
+              'data/fk/slots/cogs',
+              'data/fk/slots/fish',
+              'data/fk/slots/ghost',
+              'data/fk/slots/gold',
+              'data/fk/slots/prog',
+              'data/fk/slots/space',
+              'data/fk/slots/trap',
+              'data/fk/slots/winds',
+              'data/fk/tables',
+              'data/fk/tables/baccarat',
+              'data/fk/tables/big6',
+              'data/fk/tables/bingo',
+              'data/fk/tables/blackjack',
+              'data/fk/tables/craps',
+              'data/fk/tables/poker',
+              'data/fk/tables/roulette',
+              'data/fk/tables/war',
+              'data/fk/machines',
+              'data/fk/machines/blackjack',
+              'data/fk/machines/keno',
+              'data/fk/machines/poker']
+
+
 def main_menu():
-  while(True):
+  while True:
     print('\n\n\nMain Menu:\n'
           '[0] Four Kings\n'
           '[1] BTD6\n'
@@ -14,12 +44,14 @@ def main_menu():
 
 
 def four_kings():
-  while(True):
+  while True:
     print('\n\n\nFour Kings Companion:\n'
           '[0] View Stats for Slots\n'
           '[1] View Stats for Table Games\n'
-          '[2] Record Stats for Slots\n'
-          '[3] Record Stats for Table Games\n'
+          '[2] View Stats for Machine Games\n'
+          '[3] Record Stats for Slots\n'
+          '[4] Record Stats for Table Games\n'
+          '[5] Record Stats for Machine Games\n'
           '[.] Return to Main Menu')
     user_input = input()
     if user_input == '0':
@@ -27,9 +59,13 @@ def four_kings():
     if user_input == '1':
       fk_view_tables()
     if user_input == '2':
-      fk_record_slots()
+      fk_view_machines()
     if user_input == '3':
+      fk_record_slots()
+    if user_input == '4':
       fk_record_tables()
+    if user_input == '5':
+      fk_record_machines()
     if user_input == '.':
       return
 
@@ -38,5 +74,13 @@ def btd6():
   pass
 
 
+def init():
+  for path in DATA_PATHS_:
+    if not os.path.exists(path):
+      os.mkdir(path)
+  return
+
+
 if __name__ == '__main__':
+  init()
   main_menu()
